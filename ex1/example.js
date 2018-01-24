25f5bada929850b539cf5c0e5b1bbef33b505499
wyq <wzbaxmt@gmail.com>
2018-01-24 10:58:35 +0800

    test

--- ex1/example.js
+++ ex1/example.js
@@ -4,10 +4,7 @@ alert("hello");
 /* JS多行注释 */
 var mood = 'happy don\'t ask \"me';
 var age = -25.255555555555;
-//alert(mood);
-//alert(age);
 var married = true;
-//alert("married " + married);
 var she = Array(3);
 she[0] = "s";
 she[1] = "h";
@@ -18,7 +15,28 @@ var me = ["wyq", 25, true];
 var qtec = Array();
 qtec["laoda"] = me;
 alert(qtec["laoda"][0]);
-for(var count = 0; count < she.length; count++)
+function showArray(tempArray)
 {
-    alert(she[count]);
+    console.log("showArray " + tempArray.length);
+    for(var count = 0; count < tempArray.length; count++)
+    {
+        alert(tempArray[count]);
+    }
 }
+//showArray(mood);
+
+function convertToCelsius(temp)
+{
+    var result = temp - 32;
+    result = result / 1.8;
+    return result;
+}
+var temp_fahrenheit = 95;
+var temp_celsius = convertToCelsius(temp_fahrenheit);
+alert("celsius is " + temp_celsius);
+
+function variableTest()
+{
+    alert(age);
+}
+variableTest();
