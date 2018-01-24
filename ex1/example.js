b10ecfd8410f958263c745ea5c93b9430545b106
wyq <wzbaxmt@gmail.com>
2018-01-24 09:31:52 +0800

    ex1

---
+++ ex1/example.js
@@ -0,0 +1,24 @@
+"use strict"
+////注释注释注释
+alert("hello");
+/* JS多行注释 */
+var mood = 'happy don\'t ask \"me';
+var age = -25.255555555555;
+//alert(mood);
+//alert(age);
+var married = true;
+//alert("married " + married);
+var she = Array(3);
+she[0] = "s";
+she[1] = "h";
+she[2] = "e";
+she[6] = "2wq";
+var year = [1,2,3,4];
+var me = ["wyq", 25, true];
+var qtec = Array();
+qtec["laoda"] = me;
+alert(qtec["laoda"][0]);
+for(var count = 0; count < she.length; count++)
+{
+    alert(she[count]);
+}
