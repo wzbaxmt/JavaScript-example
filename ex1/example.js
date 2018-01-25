eb1cff25e8f90091ad55565a5106a5e349a96329
wyq <wzbaxmt@gmail.com>
2018-01-25 17:24:58 +0800

    page 42

--- ex1/example.js
+++ ex1/example.js
@@ -1,20 +1,20 @@
 "use strict"
 ////注释注释注释
-alert("hello");
+//alert("hello");
 /* JS多行注释 */
 var mood = 'happy don\'t ask \"me';
 var age = 25.55555555555;
 //内建对象
 var age = Math.round(age);
-alert(age);
+//alert(age);
 var currentDate = new Date();
 var today = currentDate.getDay();
-alert("today is " + today);
+//alert("today is " + today);
 //二维数组
 var me = ["wyq", 25, true];
 var qtec = new Array();
 qtec["laoda"] = me;
-alert(qtec["laoda"][0]);
+//alert(qtec["laoda"][0]);
 //函数调用
 function showArray(tempArray)
 {
@@ -34,11 +34,11 @@ function convertToCelsius(temp)
 }
 var temp_fahrenheit = 95;
 var temp_celsius = convertToCelsius(temp_fahrenheit);
-alert("celsius is " + temp_celsius);
+//alert("celsius is " + temp_celsius);
 
 function variableTest()
 {
     age = 18;
     alert(age);
 }
-variableTest();
+//variableTest();
